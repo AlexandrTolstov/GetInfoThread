efa19d15e255b6ede34247a23b038dff55fbaebb
AlexandrTolstov <alextav_85@mail.ru>
2019-01-27 12:08:14 +0600

    1

--- GetInfoThread/Program.cs
+++ GetInfoThread/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GetInfoThread
@@ -10,6 +11,18 @@ namespace GetInfoThread
     {
         static void Main(string[] args)
         {
+            Thread t = Thread.CurrentThread;
+
+            Console.WriteLine("Имя потока: {0}", t.Name);
+            t.Name = "Метод Main";
+            Console.WriteLine("Имя потока: {0}", t.Name);
+
+            Console.WriteLine("Запущен ли поток: {0}", t.IsAlive);
+            Console.WriteLine("Приоритет потока: {0}", t.Priority);
+            Console.WriteLine("Статус потока: {0}", t.ThreadState);
+
+            Console.WriteLine("Домен приложения: {0}", Thread.GetDomain().FriendlyName);
+            Console.ReadLine();
         }
     }
 }
